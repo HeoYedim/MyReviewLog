ce7b5ba9d9aa3efe26b4a26223f2b1c2f06ae347
HeoYedim <yeding0618@gmail.com>
2025-01-08 23:56:59 +0900

    [FEAT] New 페이지 기본적 UI 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,17 @@
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import Header from "../components/Header";
+
 const New = () => {
-  return <div>new</div>;
+  return (
+    <div>
+      <Header
+        title={"새 리뷰 작성"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default New;
