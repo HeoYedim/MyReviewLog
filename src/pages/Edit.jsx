3903dfa52edbb105a636e9c24eeed4e3abcbb78b
HeoYedim <yeding0618@gmail.com>
2025-01-26 19:57:56 +0900

    [FEAT] 리뷰 수정 UI 및 기능 구현

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,5 +1,68 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { ReviewDispatchContext, ReviewStateContext } from "../App";
+
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import Header from "../components/Header";
+
 const Edit = () => {
-  return <div>edit</div>;
+  const params = useParams();
+  const nav = useNavigate();
+  const { onDelete, onUpdate } = useContext(ReviewDispatchContext);
+  const data = useContext(ReviewStateContext);
+  const [curReviewItem, setCurReviewItem] = useState();
+
+  useEffect(() => {
+    const currentReviewItem = data.find(
+      (item) => String(item.id) === String(params.id)
+    );
+
+    if (!currentReviewItem) {
+      window.alert("존재하지 않는 리뷰입니다.");
+      nav("/", { replace: true });
+    }
+
+    setCurReviewItem(currentReviewItem);
+  }, [params.id, data]); // v7 일 땐 [params.id, data]에서 data 지우고 params.id만 남겨야 함
+
+  const onClickDelete = () => {
+    if (window.confirm("리뷰를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      // 일기 삭제 로직
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  const onSubmit = (formData) => {
+    if (window.confirm("리뷰를 정말 수정할까요?")) {
+      onUpdate(
+        params.id,
+        formData.createdDate1.getTime(), // 숫자로 변환
+        formData.createdDate2.getTime(),
+        formData.category,
+        formData.createdTitle,
+        formData.rating,
+        formData.selectedGenres,
+        formData.reviewSummary,
+        formData.reviewDetails
+      );
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div className="Edit">
+      <Header
+        title={"리뷰 수정"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"} />
+        }
+      />
+      <Editor initData={curReviewItem} onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Edit;
